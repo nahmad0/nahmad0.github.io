0c3a16bfacd9233495472bdc652c1bc9b350162d
nahmad0 <nahmad0614@gmal.com>
2025-04-17 18:30:08 -0400

    Pushnow
    Fix location for humburger menu and navigiation links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('header nav');
-
+    
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('active');
     });
