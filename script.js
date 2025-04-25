4e2c811f1231a9f23739ec616ae7d363488650c3
nahmad0 <nahmad0614@gmal.com>
2025-04-25 09:12:20 -0400

    Lets go

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ document.addEventListener("DOMContentLoaded", () => {
             });
     }
 });
+
+
+
+// Switching between tabs in index.html 
+function opentab(tabname, event) {
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
